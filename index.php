114e6fbaa30d3e4dbf7739eeb4ef3eb5e5bce72f
Vlad Anesov <hello@vladanesov.ru>
2017-07-27 17:43:35 +0300

    Добавили сложную систему выборки из разных баз данных

--- index.php
+++ index.php
@@ -11,7 +11,31 @@ define('CORE_INIT', true);
 define('PROJECT_LINK', $_SERVER['DOCUMENT_ROOT'] . '/projects/project_1635');
 require_once(PROJECT_LINK . "/mysql/connect.php");
 
-$query = BD_query("select * from `users` where 1", 1, 'test_db1');
+$serverId = 1;
+$serverDb = 'test_db1';
+
+$array = array(
+    'test_db1' => array(
+        'users' => array(
+            'search' => 'id;login;password',
+            'where' => array(
+                'test_db2' => array(
+                    'orders' => array(
+                        'self_id' => 'owner',
+                    )
+                )
+            )
+        )
+    ),
+    'test_db2' => array(
+        'orders' => array(
+            'search' => 'owner;id'
+        )
+    )
+);
+
+#$query = BD_insert($array, 'users', $serverId, $serverDb);
+$query = BD_diff_select($array, 1);
 echo '<pre>';
 var_dump($query);
 echo '</pre>';
