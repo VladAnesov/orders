bb1d56a5d8534aea4e484edda25a7ac108b0bba3
VladAnesov <hello@vladanesov.ru>
2017-08-02 01:05:45 +0300

    Добавили AJAX на нативном JS и less

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+/**
+ * Created by HOME on 27.07.2017.
+ */
+
+/*
+ Функция предназначена для выборки элемента по классу или идентификатору
+ Аналоговая из jQuery - $(element);
+ */
+function QS(element) {
+    return document.querySelectorAll(element);
+}
+
+/*
+ Функция для анимации плавного появления элемента
+ Аналоговая из jQuery - $(element).fadeIn();
+ */
+function fadeIn(element) {
+    element.style.opacity = 0;
+
+    var last = +new Date();
+    var tick = function () {
+        element.style.opacity = +element.style.opacity + (new Date() - last) / 400;
+        last = +new Date();
+
+        if (+element.style.opacity < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+        }
+    };
+
+    tick();
+}
+
+/*
+ Функция показа страниц без перезагрузки страниц.
+ */
+
+function showContent(link, element, loading, hash) {
+    event.preventDefault();
+    var cont = QS(element)[0];
+    var loading = QS(loading)[0];
+
+    cont.innerHTML = loading.innerHTML;
+
+    link = link.getAttribute("href");
+
+    var http = createRequestObject();
+    if (http) {
+        http.open('get', link);
+        http.setRequestHeader("VAHash", hash);
+        http.onreadystatechange = function () {
+            if (http.readyState == 4) {
+                cont.innerHTML = http.responseText;
+            }
+        }
+        http.send(null);
+    }
+    else {
+        document.location = link;
+    }
+}
+
+/*
+ Доп. фича, аналог AJAX
+ */
+function createRequestObject() {
+    try {
+        return new XMLHttpRequest()
+    }
+    catch (e) {
+        try {
+            return new ActiveXObject('Msxml2.XMLHTTP')
+        }
+        catch (e) {
+            try {
+                return new ActiveXObject('Microsoft.XMLHTTP')
+            }
+            catch (e) {
+                return null;
+            }
+        }
+    }
+}
+
+/*
+ Страница загружена, можно крутить спиннер.
+ */
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("page init");
+});
+
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<script src="assets/js/main.js"></script>
 <?php
 /**
  * Created by PhpStorm.
@@ -10,8 +11,38 @@ ini_set('display_errors', 'on');
 define('CORE_INIT', true);
 define('PROJECT_LINK', $_SERVER['DOCUMENT_ROOT'] . '/projects/project_1635');
 require_once(PROJECT_LINK . "/mysql/connect.php");
+require_once(PROJECT_LINK . "/functions/hash.php");
 
-$query = BD_query("select * from `users` where 1", 1, 'test_db1');
+$serverId = 1;
+$serverDb = 'test_db1';
+
+$array = array(
+    'test_db1' => array(
+        'users' => array(
+            'search' => 'id;login;password',
+            'where' => array(
+                'test_db2' => array(
+                    'orders' => array(
+                        'self_id' => 'owner',
+                    )
+                )
+            )
+        )
+    ),
+    'test_db2' => array(
+        'orders' => array(
+            'search' => 'owner;id'
+        )
+    )
+);
+
+#$query = BD_insert($array, 'users', $serverId, $serverDb);
+$query = BD_diff_select($array, 1);
 echo '<pre>';
 var_dump($query);
 echo '</pre>';
+?>
+
+<a href="test.php" onclick="<?= showContent('test.php', '.content', '.loading'); ?>">Открыть test.php</a>
+<div class="loading" style="display: none;">Загрузка</div>
+<div class="content"></div>
